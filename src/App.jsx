818f4e9ff856ea55aba9e53e071d3fb291ee099a
Aarón Adrián Huayna Silva <adrianhsilva403@gmail.com>
2025-05-16 17:49:51 +0000

    react y tailwind funcional

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">React y Tailwind</h1>
+      <p className="text-gray-600 mt-2">Ya funciona🔥</p>
+    </div>
+  )
+}
+
+export default App
